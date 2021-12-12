04f03bc8b31c5496d2ba3944966f98c3736a043f
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-12-12 16:55:45 +0500

    make it better

--- daxitsurani/settings.py
+++ daxitsurani/settings.py
@@ -168,6 +168,7 @@ AWS_QUERYSTRING_AUTH = False
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
 AWS_S3_FILE_OVERWRITE=False
 
 
